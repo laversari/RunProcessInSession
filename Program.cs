bfed3429ecc8f234a727df05fd4a60f319db3dc5
laversari <laversari@live.com>
2015-01-19 17:20:51 -0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RunProcessInSession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ARGS
+            string process = args[0];
+
+            UInt32 session = Convert.ToUInt32(args[1]);
+            
+            string processArgs = String.Empty;
+
+            //if (args.Length > 1)
+            //{
+            //    processArgs = String.Join(" ", args.Skip(1)
+            //        .Select((a) => "\"" + a + "\""));
+            //}
+           
+            IntPtr currentToken = IntPtr.Zero;
+                 
+            WinApi.OpenProcessToken(Process.GetCurrentProcess().Handle,
+                WinApi.TOKEN_DUPLICATE, out currentToken);
+
+            IntPtr newToken = IntPtr.Zero;
+
+            #region DONT TOUCH
+
+            WinApi.DuplicateTokenEx(currentToken, (uint)WinApi.ACCESS_MASK.MAXIMUM_ALLOWED,
+                IntPtr.Zero, WinApi.SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                WinApi.TOKEN_TYPE.TokenPrimary, out newToken);
+
+            //UInt32 dwSession = 3; //WinApi.WTSGetActiveConsoleSessionId(); 
+
+   
+            WinApi.SetTokenInformation(newToken, WinApi.TOKEN_INFORMATION_CLASS.TokenSessionId,
+                ref session, (UInt32) IntPtr.Size);
+
+            WinApi.PROCESS_INFORMATION pi = new WinApi.PROCESS_INFORMATION();
+            WinApi.STARTUPINFO si = new WinApi.STARTUPINFO();
+
+            string commandline = "\"" + process + "\" " + processArgs;
+
+            WinApi.CreateProcessAsUser(newToken, null, commandline, IntPtr.Zero, IntPtr.Zero,
+            false, (uint)WinApi.CreateProcessFlags.CREATE_NEW_CONSOLE,
+            IntPtr.Zero, null, ref si, out pi);
+
+            #endregion
+
+        }
+    }
+}
---
+++ WinApi.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RunProcessInSession
+{
+    public static class WinApi
+    {
+
+        #region enumerators
+
+        [Flags]
+        public enum ACCESS_MASK : uint
+        {
+            DELETE = 0x00010000,
+            READ_CONTROL = 0x00020000,
+            WRITE_DAC = 0x00040000,
+            WRITE_OWNER = 0x00080000,
+            SYNCHRONIZE = 0x00100000,
+
+            STANDARD_RIGHTS_REQUIRED = 0x000f0000,
+
+            STANDARD_RIGHTS_READ = 0x00020000,
+            STANDARD_RIGHTS_WRITE = 0x00020000,
+            STANDARD_RIGHTS_EXECUTE = 0x00020000,
+
+            STANDARD_RIGHTS_ALL = 0x001f0000,
+
+            SPECIFIC_RIGHTS_ALL = 0x0000ffff,
+
+            ACCESS_SYSTEM_SECURITY = 0x01000000,
+
+            MAXIMUM_ALLOWED = 0x02000000,
+
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000,
+            GENERIC_EXECUTE = 0x20000000,
+            GENERIC_ALL = 0x10000000,
+
+            DESKTOP_READOBJECTS = 0x00000001,
+            DESKTOP_CREATEWINDOW = 0x00000002,
+            DESKTOP_CREATEMENU = 0x00000004,
+            DESKTOP_HOOKCONTROL = 0x00000008,
+            DESKTOP_JOURNALRECORD = 0x00000010,
+            DESKTOP_JOURNALPLAYBACK = 0x00000020,
+            DESKTOP_ENUMERATE = 0x00000040,
+            DESKTOP_WRITEOBJECTS = 0x00000080,
+            DESKTOP_SWITCHDESKTOP = 0x00000100,
+
+            WINSTA_ENUMDESKTOPS = 0x00000001,
+            WINSTA_READATTRIBUTES = 0x00000002,
+            WINSTA_ACCESSCLIPBOARD = 0x00000004,
+            WINSTA_CREATEDESKTOP = 0x00000008,
+            WINSTA_WRITEATTRIBUTES = 0x00000010,
+            WINSTA_ACCESSGLOBALATOMS = 0x00000020,
+            WINSTA_EXITWINDOWS = 0x00000040,
+            WINSTA_ENUMERATE = 0x00000100,
+            WINSTA_READSCREEN = 0x00000200,
+
+            WINSTA_ALL_ACCESS = 0x0000037f
+        }
+
+        [Flags]
+        public enum CreateProcessFlags : uint
+        {
+            DEBUG_PROCESS = 0x00000001,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            CREATE_SUSPENDED = 0x00000004,
+            DETACHED_PROCESS = 0x00000008,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            NORMAL_PRIORITY_CLASS = 0x00000020,
+            IDLE_PRIORITY_CLASS = 0x00000040,
+            HIGH_PRIORITY_CLASS = 0x00000080,
+            REALTIME_PRIORITY_CLASS = 0x00000100,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_FORCEDOS = 0x00002000,
+            BELOW_NORMAL_PRIORITY_CLASS = 0x00004000,
+            ABOVE_NORMAL_PRIORITY_CLASS = 0x00008000,
+            INHERIT_PARENT_AFFINITY = 0x00010000,
+            INHERIT_CALLER_PRIORITY = 0x00020000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            PROCESS_MODE_BACKGROUND_BEGIN = 0x00100000,
+            PROCESS_MODE_BACKGROUND_END = 0x00200000,
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NO_WINDOW = 0x08000000,
+            PROFILE_USER = 0x10000000,
+            PROFILE_KERNEL = 0x20000000,
+            PROFILE_SERVER = 0x40000000,
+            CREATE_IGNORE_SYSTEM_DEFAULT = 0x80000000,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public WinApi.LUID Luid;
+            public UInt32 Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        public enum TOKEN_INFORMATION_CLASS : int
+        {
+            TokenUser = 1,
+            TokenGroups,
+            TokenPrivileges,
+            TokenOwner,
+            TokenPrimaryGroup,
+            TokenDefaultDacl,
+            TokenSource,
+            TokenType,
+            TokenImpersonationLevel,
+            TokenStatistics,
+            TokenRestrictedSids,
+            TokenSessionId,
+            TokenGroupsAndPrivileges,
+            TokenSessionReference,
+            TokenSandBoxInert,
+            TokenAuditPolicy,
+            TokenOrigin,
+            MaxTokenInfoClass
+        }
+
+        public enum WTS_INFO_CLASS
+        {
+            WTSInitialProgram = 0,
+            WTSApplicationName = 1,
+            WTSWorkingDirectory = 2,
+            WTSOEMId = 3,
+            WTSSessionId = 4,
+            WTSUserName = 5,
+            WTSWinStationName = 6,
+            WTSDomainName = 7,
+            WTSConnectState = 8,
+            WTSClientBuildNumber = 9,
+            WTSClientName = 10,
+            WTSClientDirectory = 11,
+            WTSClientProductId = 12,
+            WTSClientHardwareId = 13,
+            WTSClientAddress = 14,
+            WTSClientDisplay = 15,
+            WTSClientProtocolType = 16,
+            WTSIdleTime = 17,
+            WTSLogonTime = 18,
+            WTSIncomingBytes = 19,
+            WTSOutgoingBytes = 20,
+            WTSIncomingFrames = 21,
+            WTSOutgoingFrames = 22,
+            WTSClientInfo = 23,
+            WTSSessionInfo = 24,
+            WTSSessionInfoEx = 25,
+            WTSConfigInfo = 26,
+            WTSValidationInfo = 27,
+            WTSSessionAddressV4 = 28,
+            WTSIsRemoteSession = 29
+        };
+
+        public enum LOGON_TYPE
+        {
+            LOGON32_LOGON_INTERACTIVE = 2,
+            LOGON32_LOGON_NETWORK = 3,
+            LOGON32_LOGON_BATCH = 4,
+            LOGON32_LOGON_SERVICE = 5,
+            LOGON32_LOGON_UNLOCK = 7,
+            LOGON32_LOGON_NETWORK_CLEARTEXT = 8,
+            LOGON32_LOGON_NEW_CREDENTIALS = 9
+        };
+
+        public enum LOGON_PROVIDER
+        {
+            LOGON32_PROVIDER_DEFAULT = 0,
+            LOGON32_PROVIDER_WINNT35 = 1,
+            LOGON32_PROVIDER_WINNT40 = 2,
+            LOGON32_PROVIDER_WINNT50 = 3
+        };
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_DESCRIPTOR
+        {
+            public byte revision;
+            public byte size;
+            public short control;
+            public IntPtr owner;
+            public IntPtr group;
+            public IntPtr sacl;
+            public IntPtr dacl;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public uint nLength;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        #endregion
+
+        #region consts
+
+        public const int READ_CONTROL = 0x00020000;
+        public const int STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public const int STANDARD_RIGHTS_READ = READ_CONTROL;
+        public const int STANDARD_RIGHTS_WRITE = READ_CONTROL;
+        public const int STANDARD_RIGHTS_EXECUTE = READ_CONTROL;
+        public const int STANDARD_RIGHTS_ALL = 0x001F0000;
+        public const int SPECIFIC_RIGHTS_ALL = 0x0000FFFF;
+        public const int TOKEN_ASSIGN_PRIMARY = 0x0001;
+        public const int TOKEN_DUPLICATE = 0x0002;
+        public const int TOKEN_IMPERSONATE = 0x0004;
+        public const int TOKEN_QUERY = 0x0008;
+        public const int TOKEN_QUERY_SOURCE = 0x0010;
+        public const int TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        public const int TOKEN_ADJUST_GROUPS = 0x0040;
+        public const int TOKEN_ADJUST_DEFAULT = 0x0080;
+        public const int TOKEN_ADJUST_SESSIONID = 0x0100;
+        public const int TOKEN_ALL_ACCESS_P = (STANDARD_RIGHTS_REQUIRED |
+                                                TOKEN_ASSIGN_PRIMARY |
+                                                TOKEN_DUPLICATE |
+                                                TOKEN_IMPERSONATE |
+                                                TOKEN_QUERY |
+                                                TOKEN_QUERY_SOURCE |
+                                                TOKEN_ADJUST_PRIVILEGES |
+                                                TOKEN_ADJUST_GROUPS |
+                                                TOKEN_ADJUST_DEFAULT);
+        public const int TOKEN_ALL_ACCESS = TOKEN_ALL_ACCESS_P |
+                                            TOKEN_ADJUST_SESSIONID;
+        public const int TOKEN_READ = STANDARD_RIGHTS_READ | TOKEN_QUERY;
+        public const int TOKEN_WRITE = STANDARD_RIGHTS_WRITE |
+                                        TOKEN_ADJUST_PRIVILEGES |
+                                        TOKEN_ADJUST_GROUPS |
+                                        TOKEN_ADJUST_DEFAULT;
+        public const int TOKEN_EXECUTE = STANDARD_RIGHTS_EXECUTE;
+        public const UInt32 SE_PRIVILEGE_ENABLED_BY_DEFAULT = 0x00000001;
+        public const UInt32 SE_PRIVILEGE_ENABLED = 0x00000002;
+        public const UInt32 SE_PRIVILEGE_REMOVED = 0x00000004;
+        public const UInt32 SE_PRIVILEGE_USED_FOR_ACCESS = 0x80000000;
+        public const Int32 ANYSIZE_ARRAY = 1;
+
+        public struct TOKEN_PRIVILEGES
+        {
+            public UInt32 PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = ANYSIZE_ARRAY)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+
+
+        #endregion
+
+        #region DllImport
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(
+            IntPtr ProcessHandle,
+            UInt32 DesiredAccess,
+            out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CreateProcessAsUser(
+            IntPtr hToken,
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            uint dwDesiredAccess,
+            IntPtr lpTokenAttributes,
+            SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
+            TOKEN_TYPE TokenType,
+            out IntPtr phNewToken);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool SetTokenInformation(
+            IntPtr TokenHandle,
+            TOKEN_INFORMATION_CLASS TokenInformationClass,
+            ref UInt32 TokenInformation,
+            UInt32 TokenInformationLength);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool GetTokenInformation(
+            IntPtr TokenHandle,
+            TOKEN_INFORMATION_CLASS TokenInformationClass,
+            IntPtr TokenInformation,
+            uint TokenInformationLength,
+            out uint ReturnLength);
+        
+        [DllImport("kernel32.dll")]
+        public static extern uint WTSGetActiveConsoleSessionId();
+
+        #endregion
+
+    }
+}
